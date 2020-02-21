332ad3b6ef437f96802be3a66b3019898bfe8795
aayush.bhargava <aayush.bhargava@jungleworks.com>
2020-02-21 20:05:31 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mat-select-search';
+  foodsarray = [
+    {value: 'pizza-1', viewValue: 'Pizza is better than'},
+    {value: 'tacos-2', viewValue: 'Tacos is really tacky'},
+    {value: 'steak-10', viewValue: 'Burger asdfasdf asdf'},
+    {value: 'pizza-11', viewValue: 'Pasta adfgdfg  dfgdfg  dfgdfg sdf'},
+    {value: 'tacos-12', viewValue: 'Fries'}
+  ];
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyOwnCustomMaterialModule } from './angularMaterial.module';
+import { CustomMatSelectComponent } from './custom-mat-select/custom-mat-select.component';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomMatSelectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MyOwnCustomMaterialModule,
+    NgxMatSelectSearchModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
